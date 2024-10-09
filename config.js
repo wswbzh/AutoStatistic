378189c7756df340fdd560b3e7f279c4688edd0e
wswbzh <wswbzh@hotmail.com>
2024-10-10 02:58:06 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+网站标题="中原新能源"
+用户接口="https://xnyserver.nair.top/x/act.php?a=getalluser"
+数据接口="https://xnyserver.nair.top/x/act.php?a=getteam"
+
+//数据字段对应
+用户="nm"
+时间="created_at"
+油量="am"
+单价="cprice"
+金额="hf"
+等级="lv"
+
+
+奖金规则="if( #等级# ==## )"
